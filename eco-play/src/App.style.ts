2c7d128284aa6ec9deaca429df2d5580947437e8
José Vitor <josevitorguigarc@gmail.com>
2023-05-30 04:24:06 -0300

    first commit of eco-play reborn

---
+++ eco-play/src/App.style.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Header  = styled.div`
+width: auto;
+height: 50px;
+background-color: #65a31d;
+display: flex;
+justify-content: center;
+align-items: center;
+gap: 10px;
+`;
+
+export const OptionNav  = styled.div`
+width: fit-content;
+height: fit-content;
+border: 1px solid black;
+border-radius: 10px;
+padding: 10px;
+`
---
+++ eco-play/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+import { Header, OptionNav } from './App.style';
+
+function App() {
+  return (
+    <div className="App">
+      <Header>
+  <OptionNav>
+    Mapa da fauna
+  </OptionNav>
+  <OptionNav>
+    Quiz
+  </OptionNav>
+  <OptionNav>
+    Sobre os biomas
+  </OptionNav>
+      </Header>
+    </div>
+  );
+}
+
+export default App;
